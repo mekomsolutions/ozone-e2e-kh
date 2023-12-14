8ab09435a13a5d77da7a15b350803c2f93ed32da
Daud Kakumirizi <dkakumirizii@gmail.com>
2023-12-14 11:37:05 +0300

    OZ-437: Use a single workflow to run upon pull request(s), push(es) and manual triggers. (#29)

--- e2e/utils/configs/globalSetup.ts
+++ e2e/utils/configs/globalSetup.ts
@@ -11,12 +11,14 @@ import {
 
 dotenv.config();
 
+export const E2E_BASE_URL = `${process.env.E2E_TEST_ENVIRONMENT}` == 'uat' ? `${process.env.E2E_BASE_URL_UAT}` : `${process.env.E2E_BASE_URL_DEV}`;
+
 async function globalSetup() {
   const requestContext = await request.newContext();
   const token = Buffer.from(`${process.env.E2E_USER_ADMIN_USERNAME}:${process.env.E2E_USER_ADMIN_PASSWORD}`).toString(
     'base64',
   );
-  await requestContext.post(`${process.env.E2E_BASE_URL}/ws/rest/v1/session`, {
+  await requestContext.post(`${E2E_BASE_URL}/ws/rest/v1/session`, {
     data: {
       sessionLocation: process.env.E2E_LOGIN_DEFAULT_LOCATION_UUID,
       locale: 'en',
@@ -32,7 +34,7 @@ async function globalSetup() {
 
 export const api: WorkerFixture<APIRequestContext, PlaywrightWorkerArgs> = async ({ playwright }, use) => {
   const ctx = await playwright.request.newContext({
-    baseURL: `${process.env.E2E_BASE_URL}/ws/rest/v1/`,
+    baseURL: `${E2E_BASE_URL}/ws/rest/v1/`,
     httpCredentials: {
       username: process.env.E2E_USER_ADMIN_USERNAME ?? "",
       password: process.env.E2E_USER_ADMIN_PASSWORD ?? "",
